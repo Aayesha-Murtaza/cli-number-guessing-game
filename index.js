2037f84b5b3dba5733d602551adcc775fcb716a4
Aayesha-Murtaza <160833341+Aayesha-Murtaza@users.noreply.github.com>
2024-03-24 03:56:26 +0500

    functionality modified

--- index.js
+++ index.js
@@ -3,17 +3,27 @@ import inquirer from "inquirer";
 // 1) computer will generate a random number
 // 2) user input for guessing number
 // 3) compare user input with computer generated random number and show result
-const randomNumber = Math.floor(Math.random() * 10 + 1);
-const answer = await inquirer.prompt([
-    {
-        name: "userGuessedNumber",
-        type: "number",
-        message: "Please guess a number between 1 - 10: "
+const randomNumber = Math.floor(Math.random() * 30 + 1);
+let attempts = 0;
+while (true) {
+    const answer = await inquirer.prompt([
+        {
+            name: "userGuessedNumber",
+            type: "number",
+            message: "Please guess a number between 1 - 30: ",
+        },
+    ]);
+    if (answer.userGuessedNumber == randomNumber) {
+        console.log("Congratulations! you guessed the number.");
+        console.log("You guessed the right answer in " + attempts + " attempts.");
+        break;
     }
-]);
-if (answer.userGuessedNumber == randomNumber) {
-    console.log("Congratulations! you guessed the number.");
-}
-else {
-    console.log("You guessed wrong number");
+    else if (answer.userGuessedNumber >= randomNumber) {
+        console.log("Try a lower number.");
+        attempts++;
+    }
+    else {
+        console.log("Try a higher number.");
+        attempts++;
+    }
 }
--- index.ts
+++ index.ts
@@ -1,33 +1,34 @@
 #! /usr/bin/env node
 
-import inquirer from "inquirer"
+import inquirer from "inquirer";
 
 // 1) computer will generate a random number
 
 // 2) user input for guessing number
 
 // 3) compare user input with computer generated random number and show result
-const randomNumber = Math.floor(Math.random() * 10 + 1);
-const answer = await inquirer.prompt([
-    {
-        name : "userGuessedNumber",
-        type: "number",
-        message: "Please guess a number between 1 - 10: "
-    }
-]);
+const randomNumber = Math.floor(Math.random() * 30 + 1);
 
 let attempts = 0;
-while (true){
 
-    if(answer.userGuessedNumber == randomNumber){
-        console.log("Congratulations! you guessed the number.");
-        console.log("You guessed the right answer in "+ attempts+" attempts.");
-        break;
-    }else if (answer.userGuessedNumber >= randomNumber){
-        console.log("Try a lower number.");
-        attempts++;
-    }else{
-        console.log("Try a higher number.");
-        attempts++;
-    }
+while (true) {
+  const answer = await inquirer.prompt([
+    {
+      name: "userGuessedNumber",
+      type: "number",
+      message: "Please guess a number between 1 - 30: ",
+    },
+  ]);
+
+  if (answer.userGuessedNumber == randomNumber) {
+    console.log("Congratulations! you guessed the number.");
+    console.log("You guessed the right answer in " + attempts + " attempts.");
+    break;
+  } else if (answer.userGuessedNumber >= randomNumber) {
+    console.log("Try a lower number.");
+    attempts++;
+  } else {
+    console.log("Try a higher number.");
+    attempts++;
+  }
 }
